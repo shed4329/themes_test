eca480d42aa9a9fb3d4a8b1c38bb9fce9c6acd94
shed4329 <74101498+shed4329@users.noreply.github.com>
2022-08-07 13:41:52 +0800

    Add files via upload

--- testOne.java
+++ testOne.java
@@ -1,23 +1,24 @@
+import impl.jfxtras.styles.jmetro.ToggleSwitchSkin;
 import javafx.application.Application;
 import javafx.application.HostServices;
 import javafx.collections.FXCollections;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import jfxtras.styles.jmetro.JMetro;
 import jfxtras.styles.jmetro.Style;
+import org.controlsfx.control.ToggleSwitch;
 
+import java.io.File;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 
@@ -91,11 +92,19 @@ public class testOne extends Application {
         stage.setTitle("设置");
         stage.getIcons().add(new Image("img/setting.png"));
         stage.show();
+
+        Button home = new Button("主页            ");
+        home.setStyle("-fx-background-color:transparent");
+        home.setOnAction(actionEvent1 -> {
+            anchorPane.getChildren().clear();
+            anchorPane.getChildren().add(vBox);
+            stage.setOpacity(1);
+        });
         buttonAbout.setOnAction(actionEvent -> {
             anchorPane.getChildren().clear();
             VBox vBox1 = new VBox();
-            Button home = new Button("主页            ");
-            home.setStyle("-fx-background-color:transparent");
+
+
             Text text1 = new Text("\n关于");
             text1.setFont(Font.font("Microsoft YaHei",18));
             Button aboutOneZip = new Button("关于OneZip");
@@ -180,17 +189,11 @@ public class testOne extends Application {
                     host.showDocument("https://www.jetbrains.com/idea/");
                 });
             });
-            home.setOnAction(actionEvent1 -> {
-                anchorPane.getChildren().clear();
-                anchorPane.getChildren().add(vBox);
-                stage.setOpacity(1);
-            });
+
         });
         buttonLanguage.setOnAction(actionEvent -> {
             anchorPane.getChildren().clear();
             VBox vBox1 = new VBox();
-            Button home = new Button("主页            ");
-            home.setStyle("-fx-background-color:transparent");
             Text text1 = new Text("\n语言");
             text1.setFont(Font.font("Microsoft YaHei",18));
             Button setYourLanguage = new Button("设置语言");
@@ -208,28 +211,69 @@ public class testOne extends Application {
                 vBox2.getChildren().addAll(text2,choiceBox);
                 anchorPane.getChildren().add(vBox2);
             });
-            home.setOnAction(actionEvent1 -> {
-                anchorPane.getChildren().clear();
-                anchorPane.getChildren().add(vBox);
-                stage.setOpacity(1);
-            });
+
         });
         buttonPersonalize.setOnAction(actionEvent -> {
             anchorPane.getChildren().clear();
             VBox vBox1 = new VBox();
-            Button home = new Button("主页            ");
-            home.setStyle("-fx-background-color:transparent");
             Text text1 = new Text("\n个性化");
             text1.setFont(Font.font("Microsoft YaHei",18));
-            Button setYourBackground = new Button("设置背景");
+            Button setYourBackground = new Button("设置背景   ");
             setYourBackground.setStyle("-fx-background-color:transparent");
             Button setYourUIMode = new Button("设置UI风格");
             setYourUIMode.setStyle("-fx-background-color:transparent");
             vBox1.getChildren().addAll(home,text1,setYourBackground,setYourUIMode);
             anchorPane.getChildren().addAll(vBox1);
             stage.setOpacity(0.85);
+            AtomicBoolean everBackGround= new AtomicBoolean(false);
+            AtomicBoolean everUIMode= new AtomicBoolean(false);
+            setYourBackground.setOnAction(actionEvent1 -> {
+                setYourUIMode.setStyle("-fx-background-color:transparent");
+                setYourBackground.setStyle("-fx-background-color:#afd0ec");
+                if (everBackGround.get()||everUIMode.get()){
+                    anchorPane.getChildren().remove(1);
+                }
+                VBox vBox2 = new VBox();
+                vBox2.setLayoutX(125);
+                vBox2.prefWidthProperty().bind(anchorPane.widthProperty().subtract(125));
+                Text text2 = new Text("选择鼠标图片");
+                HBox hBox = new HBox();
+                ImageView imageView5 = new ImageView("img/Cursor1.png");
+                ImageView imageView6 = new ImageView("img/macos default.png");
+                ImageView imageView7 = new ImageView("img/Windows11 lightPointer.png");
+                ImageView imageView8 = new ImageView("img/light Arrow.png");
+                Button cursor1 = new Button("",imageView5);
+                Button cursor2 = new Button("",imageView6);
+                Button cursor3 = new Button("",imageView7);
+                Button cursor4 = new Button("",imageView8);
+                hBox.getChildren().addAll(cursor1,cursor2,cursor3,cursor4);
+                Button explore = new Button("浏览");
+                vBox2.getChildren().addAll(text2,hBox,explore);
+                vBox2.setSpacing(10);
+                anchorPane.getChildren().add(vBox2);
+                everBackGround.set(true);
+                explore.setOnAction(actionEvent2 -> {
+                    FileChooser fileChooser = new FileChooser();
+                    fileChooser.setTitle("选择图片");
+                    fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("便携式网络图形","*.png"),new FileChooser.ExtensionFilter("位图图像","*.bmp"),new FileChooser.ExtensionFilter("JPEG","*.jpeg"));
+                    File file = fileChooser.showOpenDialog(stage);
+                    if (file.exists()&&file.isFile()){
+                        System.out.println(file.getPath());
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("OK");
+                        alert.setHeaderText("set cursor ok");
+                        alert.setContentText("cursor image path:"+file.getPath());
+
+                        alert.showAndWait();
+                    }
+                });
+            });
             setYourUIMode.setOnAction(actionEvent1 -> {
+                setYourBackground.setStyle("-fx-background-color:transparent");
                 setYourUIMode.setStyle("-fx-background-color:#afd0ec");
+                if (everBackGround.get()||everUIMode.get()){
+                    anchorPane.getChildren().remove(1);
+                }
                 Text text2 = new Text("设置UI启动模式");
                 ChoiceBox choiceBox = new ChoiceBox(FXCollections.observableArrayList("JavaFX","Swing"));
                 Text text3 = new Text("设置UI主题");
@@ -248,11 +292,51 @@ public class testOne extends Application {
                         choiceBox2.setItems(FXCollections.observableArrayList("metal","radius","system","color"));
                     }
                 });
+                everUIMode.set(true);
             });
-            home.setOnAction(actionEvent1 -> {
-                anchorPane.getChildren().clear();
-                anchorPane.getChildren().add(vBox);
-                stage.setOpacity(1);
+
+        });
+        buttonUniversal.setOnAction(actionEvent -> {
+            anchorPane.getChildren().clear();
+            VBox vBox1 = new VBox();
+
+
+            Text text1 = new Text("\n通用");
+            text1.setFont(Font.font("Microsoft YaHei",18));
+
+            Button compressSetting = new Button("压缩偏好设置");
+            compressSetting.setStyle("-fx-background-color:transparent");
+            Button extractSetting = new Button("解压偏好设置");
+            compressSetting.setStyle("-fx-background-color:transparent");
+            Button viewSetting = new Button("预览偏好设置");
+            viewSetting.setStyle("-fx-background-color:transparent");
+            vBox1.getChildren().addAll(home,text1,compressSetting,extractSetting,viewSetting);
+            anchorPane.getChildren().addAll(vBox1);
+            stage.setOpacity(0.85);
+            AtomicBoolean everCompress= new AtomicBoolean(false);
+            AtomicBoolean everExtract= new AtomicBoolean(false);
+            AtomicBoolean everView= new AtomicBoolean(false);
+            compressSetting.setOnAction(actionEvent1 -> {
+                compressSetting.setStyle("-fx-background-color:#afd0ec");
+                extractSetting.setStyle("-fx-background-color:transparent");
+                viewSetting.setStyle("-fx-background-color:transparent");
+            });
+            extractSetting.setOnAction(actionEvent1 -> {
+                extractSetting.setStyle("-fx-background-color:#afd0ec");
+                compressSetting.setStyle("-fx-background-color:transparent");
+                viewSetting.setStyle("-fx-background-color:transparent");
+            });
+            viewSetting.setOnAction(actionEvent1 -> {
+                viewSetting.setStyle("-fx-background-color:#afd0ec");
+                compressSetting.setStyle("-fx-background-color:transparent");
+                extractSetting.setStyle("-fx-background-color:transparent");
+                Text text2 = new Text("为节约资源关闭预览\n");
+                ToggleSwitch viewAbleSwitch = new ToggleSwitch();
+                VBox vBox2 = new VBox();
+                vBox2.setLayoutX(125);
+                vBox2.prefWidthProperty().bind(anchorPane.widthProperty().subtract(125));
+                vBox2.getChildren().addAll(text2,viewAbleSwitch);
+                anchorPane.getChildren().add(vBox2);
             });
         });
 
